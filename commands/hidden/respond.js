0cbb168c0fadb3e2072558e7ce775454b678a3d4
ddlr <epic.emolga@gmail.com>
2017-01-09 22:26:50 +1300

    Why do I still have the ~respond command

--- commands/hidden/respond.js
+++ commands/hidden/respond.js
@@ -12,37 +12,17 @@ module.exports = {
         return new Promise(resolve => {
             switch (args) {
                 case 'heart':
-                    resolve({ message: '💚' });
-                    break;
                 case 'blue heart':
-                    resolve({ message: 'delish.' });
-                    break;
                 case 'green heart':
-                    resolve({ message: 'nom nom nom' });
-                    break;
                 case 'yellow heart':
-                    resolve({ message: 'yum!' });
-                    break;
                 case 'purple heart':
                     resolve({
                         message: 'congrats! you have just fed 1⃣ changeling.'
                     });
                     break;
-                case 'thank you':
-                    resolve({ message: 'you’re welcome 💚' });
-                    break;
-                case 'blehp':
-                    resolve({ message: 'no\nblehp *you*.' });
-                    break;
-                case 'gimme fwaf':
-                case 'gimme mwap':
-                    resolve({ message: 'no u' });
-                    break;
                 default:
-                    console.log(
-                        `${warningC('respond:')} unknown argument ` +
-                        `${args} passed`
-                    );
+                    resolve({ message: 'wha' });
+                    break;
             }
         });
     }
